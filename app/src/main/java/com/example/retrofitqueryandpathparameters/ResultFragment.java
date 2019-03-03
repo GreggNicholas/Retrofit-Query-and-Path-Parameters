c570e527d13ddf7c03282560b427a5a2cab7bf9f
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-03 16:35:19 -0500

    Creates ResultFragment

--- app/src/main/java/com/example/retrofitqueryandpathparameters/ResultFragment.java
+++ app/src/main/java/com/example/retrofitqueryandpathparameters/ResultFragment.java
@@ -1,6 +1,7 @@
 package com.example.retrofitqueryandpathparameters;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,14 +16,23 @@ public class ResultFragment extends Fragment {
 
 
     public ResultFragment() {
-        // Required empty public constructor
+    }
+
+    public static ResultFragment getInstance() {
+        ResultFragment resultFragment = new ResultFragment();
+
+        return resultFragment;
     }
 
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false);
     }
 
