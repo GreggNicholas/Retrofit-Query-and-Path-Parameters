61016c5034bd059e0871756ecd05f18e32624356
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-03 17:08:05 -0500

    Adds views to MainActivity

--- app/src/main/java/com/example/retrofitqueryandpathparameters/MainActivity.java
+++ app/src/main/java/com/example/retrofitqueryandpathparameters/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.retrofitqueryandpathparameters;
 
+import android.app.Service;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText;
+        final Button button;
+
+        editText = findViewById(R.id.edittext_main);
+        button = findViewById(R.id.button_main);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
+
+
 }
--- app/src/main/java/com/example/retrofitqueryandpathparameters/NumbersService.java
+++ app/src/main/java/com/example/retrofitqueryandpathparameters/NumbersService.java
@@ -2,11 +2,12 @@ package com.example.retrofitqueryandpathparameters;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface NumbersService {
 
-    String ENDPOINT = "0/math?json";
+    String ENDPOINT = "NumberPath/math?json";
 
-//    @GET(ENDPOINT)
-//    Call<>
+    @GET(ENDPOINT)
+    Call<NumberModel> getNumbers(@Path("numInput") String numInput);
 }
