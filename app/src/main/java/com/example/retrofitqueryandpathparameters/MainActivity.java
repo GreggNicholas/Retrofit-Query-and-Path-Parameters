4859131cbee3fef56f900b3718b2a3d34b5f3e91
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-03 17:33:29 -0500

    Creates retrofit call in MainActivity

--- app/src/main/java/com/example/retrofitqueryandpathparameters/MainActivity.java
+++ app/src/main/java/com/example/retrofitqueryandpathparameters/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.retrofitqueryandpathparameters;
 
-import android.app.Service;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +28,28 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                retrofitCall();
             }
         });
 
+    }
+
+    public void retrofitCall() {
+        String userInput = editText.getText().toString();
+        final Retrofit retrofit = RetrofitSingleton.newInstance();
+        NumbersService numberService = retrofit.create(NumbersService.class);
+        Call<NumberModel> getNumberCall = numberService.getNumbers(userInput);
+        getNumberCall.enqueue(new Callback<NumberModel>() {
+            @Override
+            public void onResponse(Call<NumberModel> call, Response<NumberModel> response) {
+                Log.d(TAG, "onResponse: " + response.body().getText());
+            }
+
+            @Override
+            public void onFailure(Call<NumberModel> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getMessage());
+            }
+        });
 
     }
 
