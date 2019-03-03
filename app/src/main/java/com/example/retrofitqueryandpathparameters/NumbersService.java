6803eff882dd25c00499b1c0513facce453643d5
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-03 16:43:54 -0500

    Creates NumberService for retrofit interface

---
+++ app/src/main/java/com/example/retrofitqueryandpathparameters/NumbersService.java
@@ -0,0 +1,12 @@
+package com.example.retrofitqueryandpathparameters;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface NumbersService {
+
+    String ENDPOINT = "0/math?json";
+
+//    @GET(ENDPOINT)
+//    Call<>
+}
