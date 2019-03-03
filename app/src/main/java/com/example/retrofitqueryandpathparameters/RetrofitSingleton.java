383bdc000ce4f533234266e6a61f1ea681be9556
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-03 16:30:36 -0500

    Adds retrofitsingleton

--- app/src/main/java/com/example/retrofitqueryandpathparameters/RetrofitSingleton.java
+++ app/src/main/java/com/example/retrofitqueryandpathparameters/RetrofitSingleton.java
@@ -1,9 +1,25 @@
 package com.example.retrofitqueryandpathparameters;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitSingleton {
+    private static Retrofit instance;
+    private static final String BASE_URL = "http://numbersapi.com/";
+
+    private RetrofitSingleton() {
+    }
 
 
-
-
+    public static Retrofit newInstance() {
+        if (instance == null) {
+            instance = new Retrofit
+                    .Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return instance;
+    }
 
 }
